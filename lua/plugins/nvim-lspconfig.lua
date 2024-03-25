eb4d4f2d2dd6dd0b050bf73d7e32c651d5d3cf0c
chenb11 <chenb11@xiaopeng.com>
2024-03-25 13:19:15 +0800

    chore: update

--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -1,15 +1,11 @@
 return {
   "neovim/nvim-lspconfig",
+  vscode = true,
   init = function()
     local keys = require("lazyvim.plugins.lsp.keymaps").get()
     -- disable a keymap
     keys[#keys + 1] = { "K", false }
     -- add a keymap
     keys[#keys + 1] = { "gh", vim.lsp.buf.hover }
-    -- keys[#keys + 1] = { "<leader>ca", false }
-    keys[#keys + 1] = { "m", mode = {
-      "n",
-      "v",
-    }, "<cmd>lua vim.lsp.buf.code_action()<CR>" }
   end,
 }
